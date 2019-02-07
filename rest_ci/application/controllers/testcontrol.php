172a84d1b905fd5980ffc570772fb3071582f925
sandi225 <43332628+sandi225@users.noreply.github.com>
2019-02-07 08:56:40 +0700

    Add files via upload

---
+++ rest_ci/application/controllers/testcontrol.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Testcontrol extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+
+    //Menampilkan data kontak
+    function index_get() {
+        $id = $this->get('id');
+        if ($id == '') {
+            $kontak = $this->db->get('telepon')->result();
+        } else {
+            $this->db->where('id', $id);
+            $kontak = $this->db->get('telepon')->result();
+        }
+        $this->response($kontak, 200);
+        $this->load->view('testview',$id);
+    }
+}
+?>
